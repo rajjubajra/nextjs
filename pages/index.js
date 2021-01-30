438945a032680cf869f0eb891b0a6f0aa1aaa7e4
rajju <vajrachara@yahoo.com>
2021-01-30 08:34:41 +0000

    updates

--- pages/index.js
+++ pages/index.js
@@ -50,6 +50,8 @@ export default function Home() {
         </div>
       </main>
 
+
+
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
