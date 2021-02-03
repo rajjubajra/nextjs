32dd733119d29a240d9652031e5eebee70bd9c74
rajju <vajrachara@yahoo.com>
2021-02-03 08:49:41 +0000

    error fixed

--- components/MusicOne/View-Button.js
+++ components/MusicOne/View-Button.js
@@ -4,7 +4,7 @@ function ViewButton({ link, name }) {
   return (
 
     <div className="w-24 text-center py-1 px-2 border-2 border-gray-700" >
-      <Link href={link ? link : '#'}>
+      <Link href={link}>
         <a>{name}</a>
       </Link>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,8 @@
-import Head from 'next/head'
 import YellowWebsite from './yellowWebsite01/index';
-import CopyRightFooter from '../components/CopyRightFooter.js/CopyRightFooter';
 
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-10">
-      <Head>
-        <title>Web Designer</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-
-      <YellowWebsite />
-
-
-      {/** copyright */}
-      <CopyRightFooter />
-
-    </div>
+    <YellowWebsite />
   )
 }
