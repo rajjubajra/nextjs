4311d1adb9c9ccf21f7cf82938cd5ca1a5fcfd2f
rajju <vajrachara@yahoo.com>
2021-02-06 11:58:20 +0000

    config file updated for remote hosting

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
-const dev = process.env.NODE_ENV === 'production'
+const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://rdbajra.com'
+export const server = dev ? 'nextjs-9uaa0arhv.vercel.app' : 'https://rdbajra.com'
 
 console.log("dev", dev, server);
